craigtaverner/neo4j

New index population
 Node changes now manifests as NodeUpdate objects, that contain the
 full set of properties and labels for the node, before and after the change.
 From the NodeUpdate each we can find the changes that are relevant to a
 particular index using forIndex(), which returns an IndexEntryUpdate. The
 IndexEntryUpdate contains only the information of interest to one particular
 index.

 This solution solution can likely cause performance degradation as we are
 looking up more properties that before, causing more db hits. We will try to
 alleviate this later by only looking up properties that are known to be of interest
 for some index.

community/consistency-check/src/main/java/org/neo4j/consistency/checking/full/PropertyAndNodeIndexedCheck.java
@@ -91,7 +91,7 @@ public class PropertyAndNodeIndexedCheck implements RecordCheck<NodeRecord, Cons
             {
                 properties = propertyReader.propertyBlocks( propertyRecs );
             }
-            int propertyId = indexRule.getSchemaDescriptor().getPropertyIds()[0]; // assuming 1 property always
+            int propertyId = indexRule.getSchemaDescriptor().getPropertyId(); // assuming 1 property always
             PropertyBlock property = propertyWithKey( properties, propertyId );
 
             if ( property == null )

community/kernel/src/main/java/org/neo4j/kernel/api/schema_new/index/NewIndexDescriptor.java
@@ -21,6 +21,7 @@ package org.neo4j.kernel.api.schema_new.index;
 
 import org.neo4j.kernel.api.TokenNameLookup;
 import org.neo4j.kernel.api.schema_new.LabelSchemaDescriptor;
+import org.neo4j.kernel.api.schema_new.SchemaUtil;
 
 import static java.lang.String.format;
 
@@ -99,5 +100,11 @@ public class NewIndexDescriptor
     public int hashCode()
     {
         return type.hashCode() & schema.hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+        return userDescription( SchemaUtil.noopTokenNameLookup );
     }
 }

community/kernel/src/main/java/org/neo4j/kernel/impl/storemigration/legacystore/v21/propertydeduplication/IndexLookup.java
@@ -64,7 +64,7 @@ class IndexLookup implements AutoCloseable
             if ( schemaRule instanceof IndexRule )
             {
                 IndexRule rule = (IndexRule) schemaRule;
-                int propertyId = rule.getSchemaDescriptor().getPropertyIds()[0]; // assuming 1 property always
+                int propertyId = rule.getSchemaDescriptor().getPropertyId(); // assuming 1 property always
                 List<IndexRule> ruleList = indexRuleIndex.get( propertyId );
                 if ( ruleList == null )
                 {

fickludd/neo4j-legacy

community/consistency-check/src/main/java/org/neo4j/consistency/checking/full/PropertyAndNodeIndexedCheck.java
@@ -91,7 +91,7 @@ public class PropertyAndNodeIndexedCheck implements RecordCheck<NodeRecord, Cons
             {
                 properties = propertyReader.propertyBlocks( propertyRecs );
             }
-            int propertyId = indexRule.schema().getPropertyIds()[0]; // assuming 1 property always
+            int propertyId = indexRule.schema().getPropertyId(); // assuming 1 property always
             PropertyBlock property = propertyWithKey( properties, propertyId );
 
             if ( property == null )

community/kernel/src/main/java/org/neo4j/kernel/api/schema_new/index/NewIndexDescriptor.java
@@ -24,6 +24,7 @@ import java.util.function.Predicate;
 import org.neo4j.kernel.api.TokenNameLookup;
 import org.neo4j.kernel.api.schema_new.LabelSchemaDescriptor;
 import org.neo4j.kernel.api.schema_new.SchemaDescriptor;
+import org.neo4j.kernel.api.schema_new.SchemaUtil;
 
 import static java.lang.String.format;
 
@@ -131,5 +132,11 @@ public class NewIndexDescriptor implements SchemaDescriptor.Supplier
     public int hashCode()
     {
         return type.hashCode() & schema.hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+        return userDescription( SchemaUtil.idTokenNameLookup );
     }
 }

community/kernel/src/main/java/org/neo4j/kernel/impl/storemigration/legacystore/v21/propertydeduplication/IndexLookup.java
@@ -64,7 +64,7 @@ class IndexLookup implements AutoCloseable
             if ( schemaRule instanceof IndexRule )
             {
                 IndexRule rule = (IndexRule) schemaRule;
-                int propertyId = rule.schema().getPropertyIds()[0]; // assuming 1 property always
+                int propertyId = rule.schema().getPropertyId(); // assuming 1 property always
                 List<IndexRule> ruleList = indexRuleIndex.get( propertyId );
                 if ( ruleList == null )
                 {